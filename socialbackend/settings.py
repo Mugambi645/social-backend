80c11a0c40eb9e22a80a1cb41598af1037429d1c
patrick mugambi <patrick@kali.pm.com>
2022-07-06 18:28:40 +0300

    set static and media files configurations

--- socialbackend/settings.py
+++ socialbackend/settings.py
@@ -144,3 +144,15 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 AUTH_PROFILE_MODULE = 'accounts.MyUser'
 
 AUTH_USER_MODEL = 'accounts.MyUser'
+STATIC_URL = 'static/'
+#media 
+MEDIA_URL = '/media/'
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_URL = '/static/'
+
+# Extra places for collectstatic to find static files.
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
--- socialbackend/urls.py
+++ socialbackend/urls.py
@@ -13,6 +13,8 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path,include
 
@@ -20,3 +22,14 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path("", include("accounts.urls")),
 ]
+
+##development media and static files settings
+if settings.DEBUG:
+    urlpatterns += static(
+        settings.MEDIA_URL, 
+        document_root=settings.MEDIA_ROOT)
+    
+if settings.DEBUG:
+    urlpatterns+= static(
+    settings.STATIC_URL, 
+    document_root = settings.STATIC_ROOT)   
