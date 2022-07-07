f457e47254710709660b112976503cdea2190993
kennedybabu <kennedynjogu8@gmail.com>
2022-07-07 12:13:59 +0300

    add corsheaders middleware

--- socialbackend/settings.py
+++ socialbackend/settings.py
@@ -56,6 +56,7 @@ INSTALLED_APPS = [
     'rest_framework',
     'knox',
     "chat",
+    "corsheaders",
   
 ]
 
@@ -67,6 +68,8 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    "corsheaders.middleware.CorsMiddleware",
+    "django.middleware.common.CommonMiddleware",
 ]
 
 ROOT_URLCONF = 'socialbackend.urls'
