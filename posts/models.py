a89027f8fb10df84db1fe514aa3fc5a9dd22584a
patrick mugambi <patrick@kali.pm.com>
2022-07-07 10:24:23 +0300

    create models post and comment

--- posts/models.py
+++ posts/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User
 # Create your models here.
-class Posts(models.Model):
+class Post(models.Model):
     """
     Model for storing post data
     """
@@ -10,8 +10,21 @@ class Posts(models.Model):
     body = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
-
+    likes = models.ManyToManyField(User, blank=True, related_name='likes')
+    dislikes = models.ManyToManyField(User, blank=True, related_name='dislikes')
     #return a string of the class object
     def __str__(self):
         return self.user + " " + self.title
+
+class Comment(models.Model):
+    """comment model
+
+    Args:
+        models (class): model to create comments
+    """
+    user = models.ForeignKey(User,on_delete=models.CASCADE)
+    post = models.ForeignKey(Post, on_delete=models.CASCADE)
+    body = models.CharField(max_length=255, blank=True)
     
+    def __str__(self):
+        return self.post + "" + self.body
